d1d45ead407f9168160423edb4b8805ab92d1ed0
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-01 19:21:22 +0530

    updated App.js

--- src/container/App.js
+++ src/container/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Sidebar from '../components/Sidebar/sidebar'
-import Form from '../components/Pages/Form/form'
 
 class App extends Component {
   render() {
     return(
 
       <BrowserRouter>
-        <Sidebar /> 
-        {/* <Form */}    
+        <Sidebar />    
       </BrowserRouter>
      
     )
